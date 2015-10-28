b5097c3388e87101b397f25cae3bfdf0bc5f199f
Cajica <jojo_213@hotmail.es>
2015-10-28 07:22:34 -0600

    examen Recu

---
+++ Mayormenor.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+	 int main()
+	 {
+	 	int N1, N2, N3,N4,N5,N6, N7, N8,N9,N10, X,Y;
+		
+		printf("dame un Numero ");
+		   scanf("%d",&N1);
+		printf("dame un Numero ");
+		   scanf("%d",&N2);	
+		printf("dame un Numero ");
+		   scanf("%d",&N3);
+		   	printf("dame un Numero ");
+		   scanf("%d",&N4);
+		   	printf("dame un Numero ");
+		   scanf("%d",&N5);
+		   	printf("dame un Numero ");
+		   scanf("%d",&N6);	
+		   	printf("dame un Numero ");
+		   scanf("%d",&N7);	
+		   	printf("dame un Numero ");
+		   scanf("%d",&N8);	
+		   	printf("dame un Numero ");
+		   scanf("%d",&N9);	
+		   	printf("dame un Numero ");
+		   scanf("%d",&N10);	
+		   		   
+		     if(N1>N2&& N1>N3 && N1>N4&& N1>N5 &&  N1> N6 &&  N1>N7 &&  N1>N8&& N1>N9 && N1>N10) 
+		printf("El valor mas alto es:  %d\n",N1);
+			
+			 if(N2>N1&& N2>N3&& N2>N4 &&  N2>N5 &&  N2>N6 && N2>N7  &&  N2>N8 && N2>N9&&  N2>N10)
+		printf("El valor mas alto es :%d\n",N2);
+		
+			 if(N3>N1&& N3>N2 && N3>N4&& N3>N5 &&  N3> N6 &&  N3>N7 &&  N3>N8&& N3>N9 && N3>N10)
+		printf("El valor mas alto es: %d\n",N3);
+			
+	 		 if(N4>N1&& N4>N2&& N4>N3 &&  N4>N5 &&  N4>N6 && N4>N7  &&  N4>N8 && N4>N9&&  N4>N10)
+		printf("El valor mas alto es: %d\n",N4);
+		
+		if(N5>N1&& N5>N2 && N5>N3&& N5>N4 &&  N5> N6 &&  N5>N7 &&  N5>N8&& N5>N9 && N1>N10) 
+		printf("El valor mas alto es: %d\n",N5);
+			
+			 if(N6>N1&& N6>N2&& N6>N3 &&  N6>N4 &&  N6>N5 && N6>N7  &&  N6>N8 && N6>N9&&  N2>N10)
+		printf("El valor mas alto es: %d\n",N6);
+		
+			 if(N7>N1&& N7>N2 && N7>N3&& N7>N4 &&  N7> N5 &&  N7>N6 &&  N7>N8&& N7>N9 && N7>N10)
+		printf("El valor mas alto es: %d\n",N7);
+			
+	 		 if(N8>N1&& N8>N2&& N8>N3 &&  N8>N4 &&  N8>N5 && N8>N6  &&  N8>N7 && N8>N9&&  N8>N10)
+		printf("El valor mas alto es: %d\n",N8);
+		
+		if(N9>N1&& N9>N2&& N9>N3 &&  N9>N4 &&  N9>N5 && N9>N6  &&  N9>N7 && N9>N8&&  N9>N10)
+		printf("El valor mas alto es:  %d\n",N9);
+		
+		if(N10>N1&& N10>N2&& N10>N3 &&  N10>N4 &&  N10>N5 && N10>N6  &&  N10>N7 && N10>N8&&  N10>N9)
+		printf("El valor mas alto es: %d\n",N10);
+		
+		
+		
+		   if(N1<N2&& N1<N3 && N1<N4&& N1<N5 &&  N1< N6 &&  N1<N7 &&  N1<N8&& N1<N9 && N1<N10) 
+		printf("El valor mas bajo  es: %d\n",N1);
+			
+			 if(N2<N1&& N2<N3&& N2<N4 &&  N2<N5 &&  N2<N6 && N2<N7  &&  N2<N8 && N2<N9&&  N2<N10)
+		printf("El valor mas bajo  es: %d\n",N2);
+		
+			 if(N3<N1&& N3<N2 && N3<N4&& N3<N5 &&  N3< N6 &&  N3<N7 &&  N3<N8&& N3<N9 && N3<N10)
+		printf("El valor mas bajo  es: %d\n",N3);
+			
+	 		 if(N4<N1&& N4<N2&& N4<N3 &&  N4<N5 &&  N4<N6 && N4<N7  &&  N4<N8 && N4<N9&&  N4<N10)
+		printf("El valor mas bajo  es: %d\n",N4);
+		
+		if(N5<N1&& N5<N2 && N5<N3&& N5<N4 &&  N5< N6 &&  N5<N7 &&  N5<N8&& N5<N9 && N1<N10) 
+		printf("El valor mas bajo  es: %d\n",N5);
+			
+			 if(N6<N1&& N6<N2&& N6<N3 &&  N6<N4 &&  N6<N5 && N6<N7  &&  N6<N8 && N6<N9&&  N2<N10)
+		printf("El valor mas bajo  es: %d\n",N6);
+		
+			 if(N7<N1&& N7<N2 && N7<N3&& N7<N4 &&  N7< N5 &&  N7<N6 &&  N7<N8&& N7<N9 && N7<N10)
+		printf("El valor mas bajo  es: %d\n",N7);
+			
+	 		 if(N8<N1&& N8<N2&& N8<N3 &&  N8<N4 &&  N8<N5 && N8<N6  &&  N8<N7 && N8<N9&&  N8<N10)
+		printf("El valor mas bajo  es: %d\n",N8);
+		
+		if(N9<N1&& N9<N2&& N9<N3 &&  N9<N4 &&  N9<N5 && N9<N6  &&  N9<N7 && N9<N8&&  N9<N10)
+		printf("El valor mas bajo  es: %d\n",N9);
+		
+		if(N10<N1&& N10<N2&& N10<N3 &&  N10<N4 &&  N10<N5 && N10<N6  &&  N10<N7 && N10<N8&&  N10<N9)
+		printf("El valor mas bajo  es: %d\n",N10);
+		
+		
+		
+	
+/*	
+	
+	if(a<b&&a<c&&a<d&&a<e&&a<f&&a<g&&a<h&&a<i&&a<j){
+	
+		printf("si el resultado fueron valores %d\n",a);
+	
+	}else if(b<a&&b<c&&b<d&&d<e&&b<f&&b<g&&b<h&&b<i&&b<j){
+
+		printf("si el resultado fueron valores %d\n",b);
+	}else if(c<b&&e<c&&c<d&&c<e&&e<f&&e<g&&e<h&&e<i&&e<j){
+	
+		printf("si el resultado fueron valores %d\n",c);
+
+	}else if(d<b&&d<c&&d<e&&d<f&&d<g&&d<h&&d<i&&d<j){
+		printf("si el resultado fueron valores %d\n",d);
+		
+	}else if(e<b&& e<c && e<d&&d<f&&d<g&&d<h&&d<i&&d<j){
+		printf("si el resultado fueron valores %d\n",e);
+	}else if(f<a&&f<b&&f<c&&f<d&&f<e&&f<g&&f<h&&f<i&&f<j){
+		printf("si el resultado fueron valores %d\n",f);
+	}else if(g<a&&g<b&&g<c&&g<d&&g<e&&g<h&&g<i&&g<j){
+		printf("si el resultado fueron valores %d\n",g);
+	}else if(h<a&&h<c&&h<d&&h<e&&h<f&&h<g&&h<i&&h<j){
+		printf("si el resultado fueron valores %d\n",h);
+	}else if(i<a&&i<c&&i<d&&i<e&&i<f&&i<g&&i<h&&i<j){
+		printf("si el resultado fueron valores %d\n",i);
+	}else if(j<a&&j<b&&j<c&&j<d&&j<e&&j<f&&j<g&&j<h&&j<i){
+	
+	printf("si el resultado fueron valores %d\n",j)
+	
+		return 0;;*/
+	
+
+	 	
+	
+}
+
+
+
+
